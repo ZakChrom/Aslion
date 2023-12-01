672c57aeebf645bdbc19b4ebdea617a305ae2946
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2023-12-01 17:26:59 +0200

    fix desktop file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp Aslion.png /usr/share/icons
-cp ./zig-out/bin/main /usr/bin/aslion
+sudo cp Aslion.png /usr/share/icons
+sudo cp ./zig-out/bin/main /usr/bin/aslion
 
 echo "[Desktop Entry]" > Aslion.desktop
 echo "Type=Application" >> Aslion.desktop
@@ -13,14 +13,14 @@ echo "Exec=aslion ./asm/triangle.asm" >> Aslion.desktop
 echo "Icon=/usr/share/icons/Aslion.png" >> Aslion.desktop
 echo "Terminal=true" >> Aslion.desktop
 echo "Categories=Other" >> Aslion.desktop
-echo "Actions=raytracer;pong;\n" >> Aslion.desktop
+echo "Actions=raytracer;pong;" >> Aslion.desktop
 
 echo "[Desktop Action raytracer]" >> Aslion.desktop
 echo "Name=Run raytracer.asm" >> Aslion.desktop
-echo "Exec=aslion ./asm/raytracer.asm\n" >> Aslion.desktop
+echo "Exec=aslion ./asm/raytracer.asm" >> Aslion.desktop
 
 echo "[Desktop Action pong]" >> Aslion.desktop
 echo "Name=Run pong.asm" >> Aslion.desktop
 echo "Exec=aslion ./asm/pong.asm" >> Aslion.desktop
 
-mv Aslion.desktop /usr/share/applications
+sudo cp Aslion.desktop /usr/share/applications
