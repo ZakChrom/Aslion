62049009e2c79d8150cc18420c72618f5c6ce3c9
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2024-04-22 15:08:37 +0300

    add args

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -xe
 
-RUSTFLAGS="-C target-cpu=native" cargo run -r -- $1
+RUSTFLAGS="-C target-cpu=native" cargo run -r -- $@
--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,61 @@ mod a8;
 mod raylib;
 mod render;
 
-use std::ffi::{c_void, CString};
+use std::{ffi::{c_uint, c_void, CString}, time::Instant};
 use a8::A8;
-use raylib::{BeginDrawing, ClearBackground, CloseWindow, Color, DrawFPS, DrawTextureEx, EndDrawing, InitWindow, LoadRenderTexture, SetConfigFlags, SetTargetFPS, UpdateTexture, Vector2, WindowShouldClose, WHITE};
+use raylib::{BeginDrawing, ClearBackground, CloseWindow, Color, ConfigFlags, DrawFPS, DrawTextureEx, EndDrawing, InitWindow, LoadRenderTexture, SetConfigFlags, SetTargetFPS, UpdateTexture, Vector2, WindowShouldClose, WHITE};
+use clap::Parser;
 
-const SCALE: usize = 8;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// File path to the asm file
+    #[arg(short, long)]
+    file: String,
+
+    /// Fps the emulator should run at. If its 0 (default) it will run as fast as possible
+    #[arg(long, default_value_t = 0)]
+    fps: i32,
+
+    #[arg(short, long, default_value_t = false)]
+    exit: bool,
+
+    #[arg(short, long, default_value_t = 8)]
+    scale: usize
+}
 
 fn main() { unsafe {
-    let mut args = std::env::args();
-    _ = args.next().unwrap(); // Program
+    let args = Args::parse();
+    if args.fps < 0 {
+        panic!("why")
+    }
 
-    let mut a8 = A8::new_from_file(&args.next().unwrap()).unwrap();
+    let mut a8 = A8::new_from_file(&args.file).unwrap();
 
     let title = CString::new("Aslion").unwrap();
-    InitWindow(108 * SCALE as i32, 108 * SCALE as i32, title.as_ptr());
-    //SetConfigFlags(ConfigFlags::VsyncHint as c_uint);
-    //SetTargetFPS(144);
+    InitWindow(108 * args.scale as i32, 108 * args.scale as i32, title.as_ptr());
+    if args.fps != 0 {
+        SetTargetFPS(args.fps);
+    }
 
     let rtexture = LoadRenderTexture(108, 108);
     let mut pixels = vec![0 as u32; 108*108];
 
     while !WindowShouldClose() {
-        //let step = Instant::now();
         while !a8.vbuf {
             a8.step();
         }
-        //let step_end = step.elapsed().as_millis();
         a8.vbuf = false;
         
-        //let render = Instant::now();
+        if args.exit { return }
+        
         render::draw(&a8, &mut pixels);
-        //println!("{}ms {}ms", step_end, render.elapsed().as_millis());
 
         UpdateTexture(rtexture.texture, pixels.as_ptr() as *const c_void);
 
         BeginDrawing();
             ClearBackground(Color { r: 0x18, g: 0x18, b: 0x18, a: 255 });
-            DrawTextureEx(rtexture.texture, Vector2 { x: 0.0, y: 0.0 }, 0.0, SCALE as f32, WHITE);
+            DrawTextureEx(rtexture.texture, Vector2 { x: 0.0, y: 0.0 }, 0.0, args.scale as f32, WHITE);
             DrawFPS(0, 0);
         EndDrawing();
     }
